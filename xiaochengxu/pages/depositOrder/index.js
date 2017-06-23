dc5996fa36d1616e8d7770ca9f4d4ec32c08d265
jerrywu001 <57242263@163.com>
2017-06-23 19:32:21 +0800

    2

--- xiaochengxu/pages/depositOrder/index.js
+++ xiaochengxu/pages/depositOrder/index.js
@@ -2,13 +2,25 @@
 var app = getApp()
 Page({
   data: {
-    flag: false,
+    flag: true,
     tabIndex: null,
     agree: false,
     search: false,
     error: false,
     loading: false,
-    animationData: {}
+    animationData: {},
+    item: [],
+    terminal_code: '',
+    terminal_name: '',
+    scroll_load_loading: false,
+    scroll_load_end: false,
+    grid0: null,
+    grid1: null,
+    grid2: null,
+    grid3: null,
+
+
+
   },
   switchTab: function (e) {
     var current = this.data.tabIndex,
@@ -76,8 +88,8 @@ Page({
     }
     setTimeout(function () {
       animation.opacity(0).step();
-      isError=false;
-      isLoading=false;
+      isError = false;
+      isLoading = false;
       this.setData({
         animationData: animation.export(),
         error: isError,
@@ -85,11 +97,82 @@ Page({
       });
     }.bind(this), 1500)
   },
+  getNetworkType: function () {
+    var that = this
+    wx.getNetworkType({
+      success: function (res) {
+        if (!res.networkType || res.networkType == 'none') {
+          app.showErrorTip(that, '当前网络不可用，请检查您的网络设置！');
+          return false;
+        }
+        return true;
+      },
+      fail: function () {
+        app.showErrorTip(that, '当前网络不可用，请检查您的网络设置！');
+        return false;
+      }
+    })
+    return true;
+  },
+  load: function () {
+    var that = this;
+    if (!that.getNetworkType()) {
+      return false
+    }
+    app.ajax('GET', '/ultrabox/storage/terminal/status?terminal_code=' + that.data.terminal_code, null, function (d) {
+      if (d.statusCode == 200) {
+        if (d.data.status == 0) {
+          var items = d.data.data;
+          that.getBoxJson(items);
+          that.setData({
+            item: items
+          })
+        }
+      }
+    })
+  },
+  getBoxJson: function (data) {
+    var box0 = null,
+      box1 = null,
+      box2 = null,
+      box3 = null,
+      item = null;
+    // box1小格 box2 中格 box3 大格 box0微格 
+    for (var i in data) {
+      item = data[i];
+      item.box_type = i;
+      if (i == 'box_type1') box1 = data['box_type1'];
+      if (i == 'box_type2') box2 = data['box_type2'];
+      if (i == 'box_type3') box3 = data['box_type3'];
+      if (i == 'box_type4') box0 = data['box_type4'];
+    }
+    if (box0.fee == 100) box0.price = 1;
+    if (box1.fee == 150) box1.price = 1.5;
+    if (box2.fee == 200) box2.price = 2;
+    if (box3.fee == 250) box3.price = 2.5;
+    this.setData({
+      grid0: box0,
+      grid1: box1,
+      grid2: box2,
+      grid3: box3,
+    })
+    console.log(box1);
+    console.log(box2);
+    console.log(box3);
+    console.log(box0);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      terminal_code: options.terminal_code,
+      terminal_name: options.terminal_name
 
+    })
+    this.load();
+    // console.log(this.data.item);
+    // this.getBoxJson(this.data.item);
   },
 
   /**
