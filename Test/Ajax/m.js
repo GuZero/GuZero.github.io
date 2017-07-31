d9a3d3d99453c625e39508bfc13ab3d5117684b2
guzero <mql_tst@163.com>
2017-07-31 20:32:23 +0800

    1

---
+++ Test/Ajax/m.js
@@ -0,0 +1,36 @@
+module.exports ={
+  getById:function(){
+    if(id) return null;
+    return document.getElementById(id);
+  },
+  showErrorTip:function(txt){
+    if(!txt) return false;
+    var mgAlert=this.getById("mgAlert");
+    myAlert.style.top=35%;
+    mgAlert.setAttribute('class','mgAlert center fixed f14 mgAlert-error showAlert');
+    mgAlert.innerHTML=txt;
+    if(window.errorTimer){
+      clearTimeout(window.errorTimer);
+      window.errorTimer = null;
+    }
+    window.errorTimer = setTimeout(function () {
+           mgAlert.setAttribute('class', 'mgAlert center fixed f14');
+           setTimeout(function () {
+               mgAlert.style.top = '-35%';
+           }, 300);
+    }, 2000);
+  },
+  showSuccessTip: function (txt) {
+        if (!txt) return false;
+        var mgAlert = this.getById("mgAlert");
+        mgAlert.style.top = '35%';
+        mgAlert.setAttribute('class', 'mgAlert center fixed f14 mgAlert-success showAlert');
+        mgAlert.innerHTML = txt;
+        setTimeout(function () {
+            mgAlert.setAttribute('class', 'mgAlert center fixed f14');
+            setTimeout(function () {
+                mgAlert.style.top = '-35%';
+            }, 300);
+        }, 2500);
+    }
+}
