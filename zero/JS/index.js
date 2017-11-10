3ae775d0c06fec2389f57e89679da7357a73ce67
guzero <mql_tst@163.com>
2017-11-10 16:43:10 +0800

    1

--- zero/JS/index.js
+++ zero/JS/index.js
@@ -1,13 +1,20 @@
-var color = 'blue';
-
-function changeColor() {
-    var anotherColor = 'red';
-
-    function swapColor() {
-        var tempColor = anotherColor;
-        anotherColor = color;
-        color = tempColor;
-    }
-    swapColor()
-}
-// 第五章
+        function getQueryStringArgs(){
+            var qs=(location.search.length>0?location.search.substring(1):''),
+                args={},
+                items=qs.length?qs.split("&"):[],
+                item=null,
+                name=null,
+                value=null,
+                i=null,
+                len=items.length;
+            for(i=0;i<len;i++){
+                item=items[i].split("=");
+                name=decodeURIComponent(item[0]);
+                value=decodeURIComponent(item[0]);
+                if(name.length){
+                    args[name]=value;
+                }
+            }
+            console.log(1)
+        }
+        getQueryStringArgs('?shelf_code=SHELF00251400002&dot_id=22')
